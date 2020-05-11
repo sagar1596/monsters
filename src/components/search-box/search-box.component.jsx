ed1a8ef44a99ccccedd29f3ee2f08526b1ed2e42
Sagar Bhat <sbhat@gluware.com>
2020-05-11 18:03:55 +0530

    Initial commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './search-box.styles.css';
+
+export const SearchBox = ({placeholder, handleChange}) => (
+    <input className="search" onChange={handleChange} type="search" placeholder={placeholder} />
+);
