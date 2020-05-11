bdd855730d374d2ab56e0fdeb7fc9f783b60574b
Sagar Bhat <sbhat@gluware.com>
2020-05-11 18:03:22 +0530

    Updates

---
+++ precache-manifest.28c372473a1a49d91369e630c3fe7f31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9242a539f0a574c8d1e32b297ab897f8",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "9b83eda7b2e74f930d6e",
+    "url": "/monsters/static/css/main.089b29a1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b83eda7b2e74f930d6e",
+    "url": "/monsters/static/js/main.052981a5.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
